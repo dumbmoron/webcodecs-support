de1f5fa8c7875ce33f103b62746cb27fa19e55fb
jj <log@riseup.net>
2025-08-31 20:27:52 +0000

    Revert "skip codec on firefox that crashes it"
    This reverts commit 7c81e81dcc8729509fff01d208ac57ac3ce2ce80.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 const load = async url => fetch(url).then(a => a.json());
-const dunce = navigator.userAgent.includes('Firefox');
 
 const supportsCodec = async (type, data) => {
     try {
@@ -28,10 +27,6 @@ const supportsVideoCodec = async (codec) => {
     const supports = {};
     for (const type of [ 'VideoDecoder', 'VideoEncoder' ]) {
         supports[type] = false;
-        if (codec === 'avc3.42001E' && dunce) {
-            continue;
-        }
-
 
         for (const [ width, height ] of resolutions) {
             try {
